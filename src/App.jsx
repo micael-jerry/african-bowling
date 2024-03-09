5a914a51ba9212f82f2678027146cd5852601b11
Micael Jerry <hei.jerry.2@gmail.com>
2024-03-09 08:03:48 +0300

    chore: enable router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./page/home/Home";
+import { ROUTE_HOME } from "./utils/routes";
 
 const App = () => {
   return (
-    <Home/>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={`${ROUTE_HOME}`}
+          element={
+            <>
+              <Home />
+            </>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;

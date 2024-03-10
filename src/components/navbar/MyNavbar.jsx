45d7ea6f26230e79c0bfe137707f16bded1b18ad
Micael Jerry <hei.jerry.2@gmail.com>
2024-03-10 20:07:52 +0300

    chore: fix navbar to top

--- src/components/navbar/MyNavbar.jsx
+++ src/components/navbar/MyNavbar.jsx
@@ -1,28 +1,26 @@
-import { Container, Navbar, Button } from "react-bootstrap";
+import { Navbar, Button } from "react-bootstrap";
 import { ROUTE_HOME } from "../../utils/routes";
 import "./MyNavbar.css";
 
 const MyNavbar = () => {
   return (
-    <Navbar className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href={`${ROUTE_HOME}`}>
-          <img
-            src="/logo.jpg"
-            className="d-inline-block align-top logo"
-            alt="AF Bowling logo"
-          />
-        </Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-          <Button
-            variant="outline-secondary"
-            onClick={() => window.location.reload()}
-          >
-            Reset
-          </Button>
-        </Navbar.Collapse>
-      </Container>
+    <Navbar fixed="top" className="bg-body-tertiary ps-4 pe-4">
+      <Navbar.Brand href={`${ROUTE_HOME}`}>
+        <img
+          src="/logo.jpg"
+          className="d-inline-block align-top logo"
+          alt="AF Bowling logo"
+        />
+      </Navbar.Brand>
+      <Navbar.Toggle />
+      <Navbar.Collapse className="justify-content-end">
+        <Button
+          variant="outline-secondary"
+          onClick={() => window.location.reload()}
+        >
+          Reset
+        </Button>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
